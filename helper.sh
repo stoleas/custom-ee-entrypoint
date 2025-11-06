aaf435126afe25ea5144d7f62a4f90c2590b9aec
Edward Quail <equail@redhat.com>
2025-11-06 16:33:41 -0500

    adding custom ee code

--- helper.sh
+++ helper.sh
@@ -1,5 +1,7 @@
 #/usr/bin/env bash
 
+source /root/ansible-buillder/bin/activate
+
 git pull && \
     ansible-builder build --tag stoleas-test-ee:latest && \
     podman tag localhost/stoleas-test-ee:latest docker.io/stoleas/stoleas-test-ee:latest && \
