b2d7f1dd578c53f9f7a4d478f268f9b6b5e0bdf1
Edward Quail <equail@redhat.com>
2025-11-06 17:29:12 -0500

    removing helper script

--- helper.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env bash
-
-source /root/ansible-builder/bin/activate
-
-/usr/bin/git pull && \
-    /root/ansible-builder/bin/ansible-builder build --tag stoleas-test-ee:latest && \
-    /usr/bin/podman tag localhost/stoleas-test-ee:latest docker.io/stoleas/stoleas-test-ee:latest && \
-    /usr/bin/podman push docker.io/stoleas/stoleas-test-ee:latest
