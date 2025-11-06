16e483a40d06f5be9e1d25baa1db7977c194b102
Edward Quail <equail@redhat.com>
2025-11-06 16:29:59 -0500

    adding custom ee code

--- context/pre-check.sh
+++ context/pre-check.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+echo "# START TEST INPUT"
+echo "### PRINTING \${ANSIBLE_EXTRA_VARS}"
 echo "${ANSIBLE_EXTRA_VARS}"
+echo "### RUNNING COMMAND env"
+env
+echo "# END TEST INPUT"
