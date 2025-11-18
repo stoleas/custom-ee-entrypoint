e25ff0e61c0c2868658dac2a6c745e635835f118
Edward Quail <equail@redhat.com>
2025-11-18 11:01:05 -0500

    adding pre-check.yml

--- context/pre-check.sh
+++ context/pre-check.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
-echo "# START TEST INPUT"
+START_END_MSG=(
+    "# STARTING VALIDATIONS ON INPUT"
+    "# VALIDATIONS COMPLETE"
+)
+VAL_TMP_DIR="/tmp/validation"
+VAL_TMP_DIR="/runner/validation/pre-check.yml"
 
-echo "# END TEST INPUT"
+printf "${START_END_MSG[0]}\n"
+mkdir  "${VAL_TMP_DIR}"
+
+ansible-runner \
+    run "${VAL_DIR}" \
+    -p  "${VAL_TMP_DIR}" \
+    --hosts localhost
+
+printf "${START_END_MSG[1]}\n"
 
 exit 0
