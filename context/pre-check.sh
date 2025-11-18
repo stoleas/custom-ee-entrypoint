8a95c9db19b2ed5538b05732391fe5f8859ef649
Edward Quail <equail@redhat.com>
2025-11-18 11:22:39 -0500

    adding inventory

--- context/pre-check.sh
+++ context/pre-check.sh
@@ -5,13 +5,17 @@ START_END_MSG=(
     "# VALIDATIONS COMPLETE"
 )
 VAL_DIR="/tmp/validation"
-VAL_PBP="/usr/local/bin/validation/pre-check.yml"
+VAL_PTH="/usr/local/bin/validation"
+VAL_PBP="${VAL_PTH}/pre-check.yml"
+VAL_INV="${VAL_PTH}/inventory"
+
+printf "%s\n" "${@}"
 
 printf "${START_END_MSG[0]}\n"
 mkdir  "${VAL_DIR}"
-ansible-runner \
-    run "${VAL_DIR}" \
-    -p  "${VAL_PBP}"
+ansible-runner  run "${VAL_DIR}" \
+    --playbook  "${VAL_PBP}" \
+    --inventory "${VAL_INV}"
 printf  "${START_END_MSG[1]}\n"
 
 exit 0
