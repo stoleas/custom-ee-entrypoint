401ca0e0e71b00a6b51a19defaedeb644d441260
Edward Quail <equail@redhat.com>
2025-11-18 11:05:01 -0500

    fixing paths

--- context/pre-check.sh
+++ context/pre-check.sh
@@ -4,17 +4,14 @@ START_END_MSG=(
     "# STARTING VALIDATIONS ON INPUT"
     "# VALIDATIONS COMPLETE"
 )
-VAL_TMP_DIR="/tmp/validation"
-VAL_TMP_DIR="/runner/validation/pre-check.yml"
+VAL_DIR="/tmp/validation"
+VAL_PBP="/runner/validation/pre-check.yml"
 
 printf "${START_END_MSG[0]}\n"
-mkdir  "${VAL_TMP_DIR}"
-
+mkdir  "${VAL_DIR}"
 ansible-runner \
     run "${VAL_DIR}" \
-    -p  "${VAL_TMP_DIR}" \
-    --hosts localhost
-
-printf "${START_END_MSG[1]}\n"
+    -p  "${VAL_PBP}"
+printf  "${START_END_MSG[1]}\n"
 
 exit 0
