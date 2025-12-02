3956f30c9f5fcae1a73ff7bf73f0918846f7169c
Edward Quail <equail@redhat.com>
2025-12-02 10:14:52 -0500

    updating logic

--- context/pre-check.sh
+++ context/pre-check.sh
@@ -9,8 +9,9 @@ VAL_PBP="${VAL_PTH}/pre-check.yml"
 VAL_INV="${VAL_PTH}/inventory"
 VAL_VAR="@/runner/env/extravars"
 
-printf "${START_END_MSG[0]}\n"
-ansible-playbook    \
-    -i "${VAL_INV}" \
-    -e "${VAL_VAR}" \
-       "${VAL_PBP}"
+#printf "${START_END_MSG[0]}\n"
+dumb-init \
+    ansible-playbook    \
+        -i "${VAL_INV}" \
+        -e "${VAL_VAR}" \
+           "${VAL_PBP}"
