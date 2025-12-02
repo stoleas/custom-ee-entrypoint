dd373520d58b5dfaf50600ea610108a04593483a
Edward Quail <equail@redhat.com>
2025-12-02 10:26:25 -0500

    updating logic

--- context/pre-check.sh
+++ context/pre-check.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
-START_END_MSG=(
-    "# STARTING VALIDATIONS ON INPUT"
-    "# VALIDATIONS COMPLETE"
-)
+#START_END_MSG=(
+#    "# STARTING VALIDATIONS ON INPUT"
+#    "# VALIDATIONS COMPLETE"
+#)
 VAL_PTH="/usr/local/bin/validation"
 VAL_PBP="${VAL_PTH}/pre-check.yml"
 VAL_INV="${VAL_PTH}/inventory"
 VAL_VAR="@/runner/env/extravars"
 
 #printf "${START_END_MSG[0]}\n"
-dumb-init \
-    ansible-playbook    \
-        -i "${VAL_INV}" \
-        -e "${VAL_VAR}" \
-           "${VAL_PBP}"
+MESSAGE="${@}"
+echo "{\"result\": \"success\", \"message\": \"${MESSAGE}\"}"
+#dumb-init \
+#    ansible-playbook    \
+#        -i "${VAL_INV}" \
+#        -e "${VAL_VAR}" \
+#           "${VAL_PBP}"
